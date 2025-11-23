dd852e038170fe266ff275dc7e68e1ffa3ff0b0f
Amelia <amelia.white@flightcentre.com>
2025-11-23 19:58:02 +1000

    feat: add nested fields for trips and trip_travellers

--- schemas/trip_traveller_schema.py
+++ schemas/trip_traveller_schema.py
@@ -1,13 +1,15 @@
 from main import ma
 from models.trip_traveller import TripTraveller
+from schemas.traveller_schema import TravellerSchema
 
 class TripTravellerSchema(ma.SQLAlchemyAutoSchema):
+    traveller = ma.Nested(TravellerSchema, only=["name", "email", "notes"])
     class Meta:
         model = TripTraveller
         load_instance = True
         ordered = True
         include_fk = True
-        fields = ("trip_id", "traveller_id", "role")
+        fields = ("trip_id", "traveller_id", "role", "traveller")
 
 trip_traveller_schema = TripTravellerSchema()
 trip_travellers_schema = TripTravellerSchema(many=True)
